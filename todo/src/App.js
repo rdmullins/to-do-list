d89f1fdbe0705145eea550d70ae3a380494a0e9c
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-10-19 17:15:53 +0000

    Initialized App.js

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+// Imports go Here
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+    return (
+        <div className="container">
+            This is my To-Do List. 
+        </div>
+    )
+};
+
+export default App;
