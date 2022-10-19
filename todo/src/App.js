66ed5da3f31eb0cd8d0a4c11b669de09b76f35c9
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-10-19 17:27:01 +0000

    Header component created

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,12 @@
 // Imports go Here
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header";
 
 function App() {
     return (
         <div className="container">
-            This is my To-Do List. 
+            <Header />
         </div>
     )
 };
