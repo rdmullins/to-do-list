1a7bb8592f36fc58b24625cafcf44b22d85aaee4
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-10-19 20:43:17 +0000

    Tweaks to text input. Bootstrap stuff.

--- todo/src/InputArea.js
+++ todo/src/InputArea.js
@@ -1,7 +1,28 @@
 function InputArea() {
+
+    let newItem = "";
+
+    function addItem(value) {
+        console.log("Item entered is: ", value);
+        
+    }
+
     return (
         <div className="row">
-            <input type="text" placeholder="Enter a New To-Do Item Here"></input>
+            <div className="input-group mb-3">
+                <input 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Enter a New To-Do Item Here"  
+                    onInput={(event) => (newItem = event.target.value)}
+                >    
+                </input>
+                <button 
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    id="btnAddNewItem"
+                    onClick={() => addItem(newItem)}>Add Item</button>
+            </div>     
         </div>
     )
 };
