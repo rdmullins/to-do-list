cdbc18f41036c17732444bc7ae98fa4a94629132
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-10-19 17:48:41 +0000

    Added text input box and placeholder

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,11 +2,13 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header";
+import InputArea from "./InputArea";
 
 function App() {
     return (
         <div className="container">
             <Header />
+            <InputArea />
         </div>
     )
 };
